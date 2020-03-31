e114637dc200ba1b556c4699a622cbcb59790f18
picpoint <picpoint@yandex.ru>
2020-04-01 01:23:46 +0300

    work with first task js

--- dist/js/selectPlan.js
+++ dist/js/selectPlan.js
@@ -1,4 +1,13 @@
 let planone = document.querySelector('.wrp__planone');
 let plantwo = document.querySelector('.wrp__plantwo');
 let planthree = document.querySelector('.wrp__planthree');
+let checkblockone = document.querySelector('.wrp__checkblockone');
+let url = 'https://raw.githubusercontent.com/picpoint/testtasks/master/datas.json';
 
+
+
+planone.addEventListener('click', () => {
+  planone.style.backgroundColor = '#ebf4f7';
+  planone.style.border = '1px solid #b8d7e2';
+  checkblockone.style.border = '5px solid #34648e';
+});

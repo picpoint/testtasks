4436c01bc5451efcfcad03a4d60647bb6b3a3508
picpoint <picpoint@yandex.ru>
2020-04-02 02:11:02 +0300

    work with styles

--- dist/js/selectPlan.js
+++ dist/js/selectPlan.js
@@ -6,8 +6,11 @@ let url = 'https://raw.githubusercontent.com/picpoint/testtasks/master/datas.jso
 
 
 
-// planone.addEventListener('click', (e) => {
-//   planone.style.backgroundColor = '#ebf4f7';
-//   planone.style.border = '1px solid #b8d7e2';
-//   checkblockone.style.border = '5px solid #34648e';    
-// });
+planone.addEventListener('click', (e) => {
+  planone.style.backgroundColor = '#ebf4f7';
+  planone.style.border = '1px solid #b8d7e2';
+  checkblockone.style.border = '5px solid #34648e';    
+  checkblockone.style.borderRadius = '50%';
+  checkblockone.style.width = '20px';
+  checkblockone.style.height = '30px';
+});

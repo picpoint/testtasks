11a87b8443f8a4365f9a4415aa6aaf038ef45455
picpoint <picpoint@yandex.ru>
2020-04-02 21:42:24 +0300

    make ajax request for option

--- src/js/selectPlan.js
+++ src/js/selectPlan.js
@@ -3,14 +3,34 @@ let plantwo = document.querySelector('.wrp__plantwo');
 let planthree = document.querySelector('.wrp__planthree');
 let checkblockone = document.querySelector('.wrp__checkblockone');
 let url = 'https://raw.githubusercontent.com/picpoint/testtasks/master/datas.json';
+let slct = document.querySelector('.wrp__slct');
+let obj;
+
+console.log(slct);
 
 
+window.addEventListener('load', () => {
+  let xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.open('GET', url);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.addEventListener('readystatechange', () => {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+      obj = xhr.response;
 
-planone.addEventListener('click', (e) => {
-  planone.style.backgroundColor = '#ebf4f7';
-  planone.style.border = '1px solid #b8d7e2';
-  checkblockone.style.border = '5px solid #34648e';    
-  checkblockone.style.borderRadius = '50%';
-  checkblockone.style.width = '20px';
-  checkblockone.style.height = '30px';
+      for(let i = 0; i < obj.nol.length; i++) {
+        console.log(obj.nol[i]);
+        let option = document.createElement('option', `value="${obj.nol[i]}"`);
+        option.innerHTML = obj.nol[i];
+        slct.appendChild(option);
+      }
+    }
+  });  
+  
+  xhr.send(); 
 });
+
+
+
+
+

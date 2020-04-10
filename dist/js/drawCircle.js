6bbc8c1a3ee1c43410c52edb2b66d3b2c6fc71da
picpoint <picpoint@yandex.ru>
2020-04-10 12:48:38 +0300

    doing task2

--- dist/js/drawCircle.js
+++ dist/js/drawCircle.js
@@ -9,22 +9,40 @@ function bigCircle() {
   ctx.lineWidth = 3;
   ctx.strokeStyle = '#34648e';
   ctx.arc(250, 250, 230, 0, Pi*2, false);
+  ctx.closePath();  
   ctx.stroke();
 }
 
 
-function smallCircle() {
+function smallCircle(x, y) {
   ctx.beginPath();
   ctx.lineWidth = 1;  
   ctx.fillStyle = '#0294bf';
-  ctx.arc(170, 170, 30, 0, Pi*2, false);
+  ctx.arc(x, y, 30, 0, Pi*2, false);
+  ctx.closePath();
   ctx.stroke();
   ctx.fill();
 }
 
 
 bigCircle();
-smallCircle();
+smallCircle(170, 170);
 
 
+canvas.addEventListener('mousemove', (e) => {  
+  let x = e.offsetX;
+  let y = e.offsetY;
+  
+  // console.log(`X - ${x}`);
+  // console.log(`Y - ${y}`);
 
+
+  if(x > 130 && y > 130) {        
+    smallCircle(190, 190);    
+  }
+  
+
+  ctx.beginPath();
+
+
+});
